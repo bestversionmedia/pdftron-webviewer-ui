fbc64772e114b9783530e5b712ec684506ad1cfc
MikeL <mlitscher@bestversionmedia.com>
2020-03-23 11:07:19 -0500

    Simplified by re-using code. Tested, and it works the same.

--- src/event-listeners/index.js
+++ src/event-listeners/index.js
@@ -13,17 +13,7 @@ import onSignatureAnnotationAdded from './onSignatureAnnotationAdded';
 import onFileAttachmentAnnotationAdded from './onFileAttachmentAnnotationAdded';
 import onAnnotationChanged from './onAnnotationChanged';
 import onStickyAnnotationAdded from './onStickyAnnotationAdded';
-import onTextHighlightAnnotationAdded from './onTextHighlightAnnotationAdded';
-import onTextHighlightAnnotationAdded2 from './onTextHighlightAnnotationAdded2';
-import onTextHighlightAnnotationAdded3 from './onTextHighlightAnnotationAdded3';
-import onTextHighlightAnnotationAdded4 from './onTextHighlightAnnotationAdded4';
-import onTextUnderlineAnnotationAdded from './onTextUnderlineAnnotationAdded';
-import onTextSquigglyAnnotationAdded from './onTextSquigglyAnnotationAdded';
-import onTextStrikeoutAnnotationAdded from './onTextStrikeoutAnnotationAdded';
-import onFreeHandAnnotationAdded from './onFreeHandAnnotationAdded';
-import onFreeHandAnnotationAdded2 from './onFreeHandAnnotationAdded2';
-import onFreeHandAnnotationAdded3 from './onFreeHandAnnotationAdded3';
-import onFreeHandAnnotationAdded4 from './onFreeHandAnnotationAdded4';
+import onAnnotationAdded from './onAnnotationAdded';
 import onFullScreenChange from './onFullScreenChange';
 import onUpdateAnnotationPermission from './onUpdateAnnotationPermission';
 import onLayoutChanged from './onLayoutChanged';
@@ -47,17 +37,7 @@ export {
   onSignatureAnnotationAdded,
   onAnnotationChanged,
   onStickyAnnotationAdded,
-  onTextHighlightAnnotationAdded,
-  onTextHighlightAnnotationAdded2,
-  onTextHighlightAnnotationAdded3,
-  onTextHighlightAnnotationAdded4,
-  onTextUnderlineAnnotationAdded,
-  onTextSquigglyAnnotationAdded,
-  onTextStrikeoutAnnotationAdded,
-  onFreeHandAnnotationAdded,
-  onFreeHandAnnotationAdded2,
-  onFreeHandAnnotationAdded3,
-  onFreeHandAnnotationAdded4,
+  onAnnotationAdded,
   onFullScreenChange,
   onUpdateAnnotationPermission,
   onLayoutChanged,
